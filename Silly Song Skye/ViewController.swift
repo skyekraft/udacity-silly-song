d215b4b9e4ec680256be5e794e86081dda74fcef
Skye <skraft@aftercollege.com>
2016-09-18 19:19:24 -0700

    Not sure why my earlier commit contained the unedited files…

--- Silly Song Skye/ViewController.swift
+++ Silly Song Skye/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var lyricsView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(sender: AnyObject) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    
+    @IBAction func displayLyrics(sender: AnyObject) {
+        if let nameField = nameField.text {
+        lyricsView.text = lyricsForName(bananaFanaTemplate, fullName: nameField)
+    }
 
 }
 
+func shortNameFromName(fullName: String) -> String {
+    let stepOne = fullName.lowercaseString.stringByFoldingWithOptions(.DiacriticInsensitiveSearch, locale: nil)
+    let vowels = NSCharacterSet(charactersInString: "aeiouy")
+    
+    if let firstVowel = stepOne.rangeOfCharacterFromSet(vowels, options: .CaseInsensitiveSearch) {
+        return stepOne.substringFromIndex(firstVowel.startIndex)
+    }
+    return stepOne
+}
+
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joinWithSeparator("\n")
+
+func lyricsForName(bananaFanaTemplate: String, fullName: String) -> String {
+    let stepOne = shortNameFromName(fullName)
+    let lyrics = bananaFanaTemplate.stringByReplacingOccurrencesOfString("<FULL_NAME>", withString:  fullName).stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: stepOne)
+    
+    return lyrics
+}
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
